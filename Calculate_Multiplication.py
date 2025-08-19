abf0df9c1ab0201dc30719ca94bff2c13ccfc077
shihab-md-akash <shihabmdakash10@gmail.com>
2025-08-19 23:30:37 +0600

    Add script for basic arithmetic operations

---
+++ Calculate_Multiplication.py
@@ -0,0 +1,11 @@
+num1=input("enter first number: ")
+num2=input("enter second number: ")
+print(f"Multiplication result: {int(num1) * int(num2)}")
+# This script takes two numbers as input from the user and prints their multiplication result.
+print(f"Division result:{int(num1) / int(num2)  if int(num2) != 0 else 'undefined'  }")
+# This script also calculates the division of the two numbers, handling division by zero.
+print(f"Addition result: {int(num1) + int(num2)}")
+# This script calculates the addition of the two numbers.
+print(f"Subtraction result: {int(num1) - int(num2)}")
+# This script calculates the subtraction of the two numbers.    
+# This script performs basic arithmetic operations: multiplication, division, addition, and subtraction.
