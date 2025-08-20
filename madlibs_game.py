ab2b5a27b513794f401f76dafb0dd5e676ec033e
shihab-md-akash <shihabmdakash10@gmail.com>
2025-08-20 19:44:20 +0600

    Add madlibs game script

---
+++ madlibs_game.py
@@ -0,0 +1,9 @@
+adjective = input("Enter an adjective: ")
+animal1 = input("Enter the first animal: ")
+animal2 = input("Enter the second animal: ")    
+
+
+print (f"Today I went to a {adjective}  Zoo")
+print(f"in the zoo I saw a {animal1} and a {animal2}")
+# This script generates a simple madlib story using user-provided words.
+
